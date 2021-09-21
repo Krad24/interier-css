baff029f83a5ed215d054d9e758c8a2a947500a5
krad <marhipov240692@gmail.com>
2021-09-21 17:31:14 +0300

    master

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function(){
 
+    $('.slick').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick__arrow slick__arrowprev"><img src="images/arrow-prev.svg" alt="arrow-prev"></button>',
+        nextArrow: '<button type="button" class="slick__arrow slick__arrownext"><img src="images/arrow-next.svg" alt="arrow-next"></button>',
+      });
 });
