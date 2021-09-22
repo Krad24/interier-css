6bc926a3c5f0f7b956dd2993133f29b826a1e6c8
krad <marhipov240692@gmail.com>
2021-09-22 14:52:40 +0300

    master

--- js/main.js
+++ js/main.js
@@ -4,5 +4,19 @@ $(function(){
         dots: true,
         prevArrow: '<button type="button" class="slick__arrow slick__arrowprev"><img src="images/arrow-prev.svg" alt="arrow-prev"></button>',
         nextArrow: '<button type="button" class="slick__arrow slick__arrownext"><img src="images/arrow-next.svg" alt="arrow-next"></button>',
+  
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              dots: false,
+              arrows: false,
+              autoplay: true,
+              fade: true,
+            }
+          }
+        ],
       });
+
+
 });
